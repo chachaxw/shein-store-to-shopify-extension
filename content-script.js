8412362364219de3eb3f16ff1d3e0d820f00cf36
Chacha <867571123@qq.com>
2021-10-23 17:08:00 +0800

    feat: add product view and save button

--- content-script.js
+++ content-script.js
@@ -1,20 +1,22 @@
-let button = document.createElement("button");
-
-button.innerText = "Grab";
-button.style.position = "fixed";
-button.style.right = "4px";
-button.style.top = "4px";
-button.style.zIndex = 9999;
-
-// …and register a listener for when that button is clicked
-button.addEventListener("click", getPageProductInfo);
-document.querySelector("body").appendChild(button);
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log("request", request);
   console.log("sender", sender);
   console.log("sendResponse", sendResponse);
 });
+
+initGrabButton();
+initContainerView();
+
+function initGrabButton() {
+  const grabButton = document.createElement("button");
+
+  grabButton.innerText = "Grab";
+  grabButton.classList.add("grab-button");
+
+  // …and register a listener for when that button is clicked
+  grabButton.addEventListener("click", getPageProductInfo);
+  document.querySelector("body").appendChild(grabButton);
+}
 
 // The body of this function will be execuetd as a content script inside the current page
 function getPageProductInfo() {
@@ -48,6 +50,7 @@ function getPageProductInfo() {
     .map((item) => {
       return item.firstChild.src;
     });
+  const formatImgList = Array.from(new Set(productImgList));
 
   const productColorNodeList = productInfoWrapper.querySelectorAll(
     ".product-intro__color-choose .product-intro__color-radio img"
@@ -59,14 +62,88 @@ function getPageProductInfo() {
     });
 
   console.log(
-    Array.from(new Set(productImgList)),
+    formatImgList,
     productInfoTitle,
     productInfoSku,
     productInfoPrice,
     productInfoSizeList,
     productColorList
   );
+
+  showContainer();
+  initProductInfoTitle(productInfoTitle);
+  initProductImg(formatImgList);
+  initSaveButton();
 }
 
-// Build product view
-function initProductView() {}
+// Build container view
+function initContainerView() {
+  const container = document.createElement("div");
+  const cancelButton = document.createElement("button");
+
+  cancelButton.innerText = "Close";
+  cancelButton.classList.add("cancel-button");
+  cancelButton.addEventListener("click", closeContainer);
+
+  container.appendChild(cancelButton);
+  container.classList.add("shopify-container");
+  document.body.appendChild(container);
+}
+
+// make container visible
+function showContainer() {
+  const container = document.querySelector(".shopify-container");
+  container.style.right = "0";
+}
+
+// make container hidden
+function closeContainer() {
+  const container = document.querySelector(".shopify-container");
+  container.style.right = "-250px";
+}
+
+function initProductInfoTitle(productInfoTitle) {
+  const container = document.querySelector(".shopify-container");
+  const title = document.createElement("div");
+
+  title.innerText = productInfoTitle;
+  title.classList.add("product-title");
+  container.appendChild(title);
+}
+
+function initProductImg(productImgList) {
+  const container = document.querySelector(".shopify-container");
+  const imgContainer = document.createElement("div");
+
+  productImgList.map((item) => {
+    const img = document.createElement("img");
+    img.src = item;
+    imgContainer.appendChild(img);
+  });
+
+  imgContainer.classList.add("product-img");
+  container.appendChild(imgContainer);
+}
+
+function initProductInfoSize(productInfoSize) {
+  const container = document.querySelector(".shopify-container");
+  const size = document.createElement("div");
+
+  size.classList.add("product-size");
+  container.appendChild(title);
+}
+
+// Build container view
+function initSaveButton() {
+  const saveButton = document.createElement("button");
+
+  saveButton.innerText = "Save";
+  saveButton.classList.add("save-button");
+  saveButton.addEventListener("click", saveProduct);
+
+  container.appendChild(saveButton);
+}
+
+function saveProduct() {
+  console.log("保存");
+}
